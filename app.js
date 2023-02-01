2f78e030b55bd97d51575e447164c5086d7f79c7
DESKTOP-1KR2NBI\timos <t.diakakis@pobuca.com>
2023-02-02 00:07:44 +0200

    1/2 homepage changes

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const backgroundImage = document.querySelector(".background-image");
-const headerLogo = document.querySelector(".header-logo");
+const backgroundImage = document.querySelector('.background-image');
+const headerLogo = document.querySelector('.header-logo');
+const bottomImages = document.querySelector('.bottom-images');
+const socialIcons = document.querySelector('.social-icons');
+const scrollingHeader = document.querySelector('.header-container-scrolling');
+const textWhiteLine = document.querySelector('.header-container--text-line__parent');
+let backgroundImagePos = backgroundImage.getBoundingClientRect();
 
 window.addEventListener("scroll", function () {
-  let x = backgroundImage.getBoundingClientRect();
+  let headerLogoPos = backgroundImage.getBoundingClientRect();
+  // console.log(backgroundImage.clientHeight);
+  // console.log(headerLogoPos.bottom);
+  console.log(headerLogoPos.bottom);
 
-  if (x.bottom < headerLogo.clientHeight + 50) {
-    headerLogo.classList.add("absolute-class");
-  } else {
-    headerLogo.classList.remove("absolute-class");
+  // tablet and above
+  if(window.innerWidth > 960) {
+    if(headerLogoPos.bottom + backgroundImage.clientHeight < backgroundImage.clientHeight) {
+      bottomImages.classList.add('fixed-images-class');
+      socialIcons.classList.add('fixed-social-class');
+      textWhiteLine.classList.add('fixed-white-bar-class');
+      scrollingHeader.classList.add('show');
+    } else {
+      bottomImages.classList.remove('fixed-images-class');
+      socialIcons.classList.remove('fixed-social-class');
+      textWhiteLine.classList.remove('fixed-white-bar-class');
+      scrollingHeader.classList.remove('show');
+    }
   }
 });
