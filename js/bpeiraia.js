6610aee3aa9195ba44bc5de0305de40a73530ec7
DESKTOP-1KR2NBI\timos <t.diakakis@pobuca.com>
2023-02-11 18:23:34 +0200

    11/2 allages

--- js/bpeiraia.js
+++ js/bpeiraia.js
@@ -1,5 +1,5 @@
 // Desktop Images
-const imagesDesktopAccordion = document.querySelectorAll('.inside-pages--info__images-photo');
+const imagesDesktopAccordion = document.querySelectorAll('.image-container');
 // Desktop Accordions
 const perioxesAccordionsDesktop = document.querySelectorAll('.inside-pages--info__phrases--container');
 
@@ -7,6 +7,7 @@ const perioxesAccordionsDesktop = document.querySelectorAll('.inside-pages--info
 const imagesMobileAccordion = document.querySelectorAll('.inside-pages--info--mobile__image-phrase--images');
 // Desktop Accordions
 const perioxesAccordionsMobile = document.querySelectorAll('.inside-pages--info--mobile__image-phrase--container');
+const perioxesAccordionsMobileContainer = document.querySelector('.inside-pages--info--mobile');
 
 
 
@@ -14,13 +15,20 @@ const perioxesAccordionsMobile = document.querySelectorAll('.inside-pages--info-
 
 imagesDesktopAccordion.forEach( (image) => {
     image.addEventListener('click', function() {
+        console.log(image.id);
+        image.classList.toggle('active-image')
+        
+        imagesDesktopAccordion.forEach( (imageNew) => {
+            if(imageNew.id !== image.id) {
+                imageNew.classList.remove('active-image');
+            }
+        })
         perioxesAccordionsDesktop.forEach( (perioxi) => {
             if(image.id === perioxi.id) {
                 perioxi.classList.toggle('show');
-
             }
             else {
-                perioxi.classList.remove('show')
+                perioxi.classList.remove('show');
             }
         })
     })
@@ -32,10 +40,20 @@ imagesMobileAccordion.forEach( (image) => {
     image.addEventListener('click', function() {
         perioxesAccordionsMobile.forEach( (perioxi) => {
             if(image.id === perioxi.id) {
+                console.log(image.id, perioxi.id);
+                if(image.id === "keratsiniImageAccordion" && !(perioxi.classList.contains('show'))) {
+                    console.log("ttrue");
+                    perioxesAccordionsMobileContainer.style.paddingBottom = "500px";
+                }
+                else {
+                    perioxesAccordionsMobileContainer.style.paddingBottom = "200px";
+                }
+                image.scrollIntoView(true)
                 perioxi.classList.toggle('show');
 
             }
             else {
+                // perioxi.style.marginBottom = "0";
                 perioxi.classList.remove('show')
             }
         })
