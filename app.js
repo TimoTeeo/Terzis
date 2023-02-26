f95980a69aaa0a3c3881234677aaadd3220d35a8
IraklisAsimakopoulos <109203661+IraklisAsimakopoulos@users.noreply.github.com>
2023-02-27 00:57:17 +0200

    27/2

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const backgroundImage = document.querySelector('.background-image');
-const headerLogo = document.querySelector('.header-logo');
-const bottomImages = document.querySelector('.bottom-images');
-const socialIcons = document.querySelector('.social-icons');
-const scrollingHeader = document.querySelector('.header-container-scrolling');
-const textWhiteLine = document.querySelector('.header-container--text-line__parent');
-let backgroundImagePos = backgroundImage.getBoundingClientRect();
+const backgroundImage = document.querySelector(".background-image");
+const headerLogo = document.querySelector(".header-logo");
+const bottomImages = document.querySelector(".bottom-images");
+const socialIcons = document.querySelector(".social-icons");
+const scrollingHeader = document.querySelector(".header-container-scrolling");
+const textWhiteLine = document.querySelector(
+  ".header-container--text-line__parent"
+);
 
 window.addEventListener("scroll", function () {
   let headerLogoPos = backgroundImage.getBoundingClientRect();
@@ -12,38 +13,51 @@ window.addEventListener("scroll", function () {
   // console.log(headerLogoPos.bottom);
 
   // tablet and above
-  if(window.innerWidth > 990) {
-    if(headerLogoPos.bottom + backgroundImage.clientHeight - 150 < backgroundImage.clientHeight) {
-      bottomImages.classList.add('fixed-images-class');
-      socialIcons.classList.add('fixed-social-class');
-      textWhiteLine.classList.add('fixed-white-bar-class');
-      scrollingHeader.classList.add('show');
+  if (window.innerWidth > 1450) {
+    if (
+      headerLogoPos.bottom + backgroundImage.clientHeight - 150 <
+      backgroundImage.clientHeight
+    ) {
+      bottomImages.classList.add("fixed-images-class");
+      socialIcons.classList.add("fixed-social-class");
+      textWhiteLine.classList.add("fixed-white-bar-class");
+      scrollingHeader.classList.add("show");
     } else {
-      bottomImages.classList.remove('fixed-images-class');
-      socialIcons.classList.remove('fixed-social-class');
-      textWhiteLine.classList.remove('fixed-white-bar-class');
-      scrollingHeader.classList.remove('show');
+      bottomImages.classList.remove("fixed-images-class");
+      socialIcons.classList.remove("fixed-social-class");
+      textWhiteLine.classList.remove("fixed-white-bar-class");
+      scrollingHeader.classList.remove("show");
     }
-  }
-  else {
-    if(window.scrollY > 10) {
-      scrollingHeader.classList.add('show');
-      if(headerLogoPos.bottom + backgroundImage.clientHeight - 150 < backgroundImage.clientHeight) {
-        textWhiteLine.classList.add('fixed-white-bar-class');
-      }
-      else {
-        textWhiteLine.classList.remove('fixed-white-bar-class');
+  } else {
+    if (window.scrollY > 10) {
+      scrollingHeader.classList.add("show");
+      if (
+        headerLogoPos.bottom + backgroundImage.clientHeight - 150 <
+        backgroundImage.clientHeight
+      ) {
+        textWhiteLine.classList.add("fixed-white-bar-class");
+      } else {
+        textWhiteLine.classList.remove("fixed-white-bar-class");
       }
     } else {
-      scrollingHeader.classList.remove('show');
+      scrollingHeader.classList.remove("show");
     }
   }
 });
 
 window.addEventListener("resize", function () {
-  if(window.innerWidth < 990) {  
-    if(bottomImages.classList.contains('fixed-images-class')) {
-      bottomImages.classList.remove('fixed-images-class')
+  let headerLogoPos = backgroundImage.getBoundingClientRect();
+  if (window.innerWidth < 1450) {
+    if (bottomImages.classList.contains("fixed-images-class")) {
+      bottomImages.classList.remove("fixed-images-class");
+    }
+  } else {
+    if (
+      !bottomImages.classList.contains("fixed-images-class") &&
+      headerLogoPos.bottom + backgroundImage.clientHeight - 150 <
+        backgroundImage.clientHeight
+    ) {
+      bottomImages.classList.add("fixed-images-class");
     }
   }
 });
